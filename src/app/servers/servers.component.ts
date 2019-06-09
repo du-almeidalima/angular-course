2e72f6e73cba38bb1e838c9aa090e810d58ecc79
Xenosgrilda <dualmeidalima@gmail.com>
2019-06-09 19:14:51 -0300

    Section 2: Working with Component Templates

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -16,6 +16,5 @@ export class ServersComponent implements OnInit {
 
 
 /** Another way of creating a component is through CLI = gn generate component [component name]
- * This will also automatically include this new component into app.component.ts => declarations: []
+ * This will also automatically include this new component into app.component.ts => declarations: [ Components ]
  */
-
