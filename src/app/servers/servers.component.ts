8d21e11a7d8cfd37c8506e2b6486dba32e2decf6
Xenosgrilda <dualmeidalima@gmail.com>
2019-06-10 17:44:57 -0300

    Section 2: Passing and Using Data with Event Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
 
   public allowServers = false;
   public serverCreationStatus = 'No server was created';
+  public serverName: string;
 
   constructor() {
     setTimeout(() => {
@@ -19,11 +20,19 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 
-  onCreateServer(){
+  onCreateServer() {
     this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (event.target as HTMLInputElement).value;
   }
 }
 
 /**
- * onCreateServer = Just a regular function, it's a good practice to name with the prefix "on" functions that will be called from a template
+ * We can pass a "event" just like JavaScript, but since TypeScript a typed "language" we need to specify which element this even will be.
+ * We can do it by "casting" the event to a element (<HTMLInputElement>event.target).value
+ * Or We can assert it as shown above
+ *
+ * Read more of Assertions and Casting on https://basarat.gitbooks.io/typescript/docs/types/type-assertion.html
  */
