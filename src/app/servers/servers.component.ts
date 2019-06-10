0333f7280e3d1e13615f04a69786eaff93c5dff1
Xenosgrilda <dualmeidalima@gmail.com>
2019-06-10 19:44:09 -0300

    Using ngIf to Output Data Conditionally

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,8 +10,11 @@ export class ServersComponent implements OnInit {
   public allowServers = false;
   public serverCreationStatus = 'No server was created';
   public serverName: string;
+  public serverCreated: boolean;
 
   constructor() {
+    this.serverCreated = false;
+
     setTimeout(() => {
       this.allowServers = true;
     }, 4000);
@@ -21,6 +24,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = `Server: ${this.serverName} was created!`;
   }
 
@@ -29,10 +33,3 @@ export class ServersComponent implements OnInit {
   }
 }
 
-/**
- * We can pass a "event" just like JavaScript, but since TypeScript a typed "language" we need to specify which element this even will be.
- * We can do it by "casting" the event to a element (<HTMLInputElement>event.target).value
- * Or We can assert it as shown above
- *
- * Read more of Assertions and Casting on https://basarat.gitbooks.io/typescript/docs/types/type-assertion.html
- */
