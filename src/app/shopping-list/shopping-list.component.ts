fc144d53de825e65563139b550d677acbac677af
Xenosgrilda <dualmeidalima@gmail.com>
2019-06-20 15:27:56 -0300

    Section 3: Working on the ShoppingListComponent

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  public ingredients = [
+
+  ];
 
   constructor() { }
 
