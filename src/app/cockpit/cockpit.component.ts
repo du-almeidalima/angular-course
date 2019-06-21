fc3e7874471ba4fab10efd7f227785d462fad52e
Xenosgrilda <dualmeidalima@gmail.com>
2019-06-21 11:51:23 -0300

    Section 5: Splitting Apps into Components

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: './cockpit.component',
+    selector: 'app-cockpit'
+})
+export class Cockpit {
+
+    public newServerName = '';
+    public newServerContent = '';
+
+    onAddServer() {
+        this.serverElements.push({
+          type: 'server',
+          name: this.newServerName,
+          content: this.newServerContent
+        });
+    }
+    
+    onAddBlueprint() {
+        this.serverElements.push({
+            type: 'blueprint',
+            name: this.newServerName,
+            content: this.newServerContent
+        });
+    }
+}
