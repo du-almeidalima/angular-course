f4187d0ad515b34d90d37797980dac0505127016
Xenosgrilda <dualmeidalima@gmail.com>
2019-06-21 16:52:34 -0300

    Using Local References in Templates

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -9,35 +9,18 @@ export class CockpitComponent {
     @Output() public serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
     @Output('bpCreated') public blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
-    // Properties / Fields
-    public newServerName = '';
-    public newServerContent = '';
+    onAddServer(nameInput: HTMLInputElement, contentInput: HTMLInputElement) {
 
-    onAddServer() {
-      this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+      this.serverCreated.emit({
+          serverName: nameInput.value,
+          serverContent: contentInput.value
+      });
     }
 
-    onAddBlueprint() {
-      this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+    onAddBlueprint(nameInput: HTMLInputElement, contentInput: HTMLInputElement) {
+      this.blueprintCreated.emit({
+          serverName: nameInput.value,
+          serverContent: contentInput.value
+      });
     }
 }
-
-/**
- * What if we wanted to pass data upwards, from the child component to the parent component whenever some event happened ?
- * One way of doing this is declaring a function in the child component that will call the parent component passing the values.
- * In this case, we'll add 2 functions or events that when triggered, will call the AppComponent functions and will pass data.
- *
- * NOTE: to make something an event that can get triggered, we need to assign it as an "EventEmitter"
- * Pass the generics of what kind of data it will emit "<>"
- * Then, just call the constructor by placing () at the end
- *
- * The "EventEmitter" is an object in the Angular framework that allows you to emit you own events
- *
- * FINALLY, we've created the event, assigned the type of data it'll emit, now we just need to say when it will be triggered
- * We can do this by adding this property to a function or a routine just like in the example above
- * Also, we need to say the data that this event will pass
- *
- * LAST OF ALL, the same way we used @Input inform Angular that the property "element" of server-element would receive a value from
- * "outside", we also need inform Angular that this property ("serverCreated", "blueprintCreated") will send data to outside
- * We can do this by adding the Decorator @Output, and we can also assign an alias to it, just like in @Input
-*/
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -16,23 +16,3 @@ export class ServerElementComponent implements OnInit {
   }
 
 }
-
-/** In the server-element.component.html we're trying to access a js object "element", but it doesn't exists in our ts file yet.
- *  So lets create this property "element" in our class !
- *
- *  But, this property is still only accessible only inside this component :c
- *  In Angular, we need to explicitly say that a property is allowed to be accessible by parents components
- *  To do this, we need to add a Decorator to the property: "@Input()" and import it
- *
- *  NOTE: This property "element" will only be visible by the component that is implementing this component (ServerElementComponent)
- *  through the selector "app-server-element"
- *
- *  NOTE: We can also expose our property/field 'element' with an alias, as shown above
- *
- *  NOTE: When a property/field is declared with the decorator @Input() you don't need to initialize it on constructor
- *
- *  View Encapsulation: Angular enforces a way of each component has its own CSS style applied to it, but how does Angular does that?
- *  It actually create attributes, that can be seen on the inspection tool, to identify the component html, so its CSS is only applied
- *  to it. We can change this behaviour in the @Component decorator by adding the "encapsulation" property and with the "ViewEncapsulation"
- *  value
-*/
