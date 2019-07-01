ce2964f10d51e93fc24e9bbc293e45ae4a4e7a69
Xenosgrilda <dualmeidalima@gmail.com>
2019-07-01 20:13:51 -0300

    Section 7: Understanding ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ export class AppComponent {
   oddNumbers = [1, 3, 5];
   evenNumbers = [2, 4];
   onlyOdd = false;
+  dummyValue = 0;
 }
+
+/**
+ * Imagine that we wanted to display something for specif values from dummyValue, we could use ngSwitch, take a look on app.compent.html
+*/
