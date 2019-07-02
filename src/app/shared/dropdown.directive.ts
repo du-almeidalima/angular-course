68de1e5bfafe9bd6a01ce8b42b18b09c7b127cb8
Xenosgrilda <dualmeidalima@gmail.com>
2019-07-02 17:29:13 -0300

    Section 8: Closing the Dropdown from Anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -8,7 +8,7 @@ export class DropdownDirective {
 
   private show = false;
 
-  constructor(private renderer: Renderer2){}
+  constructor( private elRef: ElementRef, private renderer: Renderer2 ) {}
 
   @HostListener('click')
   public onClick(){
@@ -21,6 +21,22 @@ export class DropdownDirective {
     }
   }
 
+  // For closing the Dropdown from Anywhere
+  @HostListener('document:click',['$event'])
+  public onOuterClick(event: Event){
+
+    console.log(this.show);
+
+    if (! this.elRef.nativeElement.contains(event.target)){
+      this.renderer.removeClass(this.dropdownMenu, 'show');
+    }
+    this.show = false;
+  }
+
 }
 
-// This is a directive to enable Bootstrap dropdown
+/** This is a directive to enable Bootstrap dropdown
+ * For closing it whenever user clicks outside what we did was, we actually attached an event to the dom itself whenever
+ * it was clicked, and passed this event to our function "onOuterClick"
+ * Then we check if the element our decorator sits on DOES NOT have the target we simple hide it
+ */
