cb65973c36dbabc0a75a817c77b5dc602a021818
Xenosgrilda <dualmeidalima@gmail.com>
2019-07-13 23:10:55 -0300

    Section 11: Using Relative Pathis in Programmatic Navigation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,23 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
+  public onReloadPage() {
+    this.router.navigate(['/servers'], {relativeTo: this.route});
+  }
 }
+
+/** In this component we're using relative path and it's not throwing an error like using relative paths in RouterLink ( which would add,
+ * the path to the url), what happens is, when the Router is used programatically, it doesn't know where it's sitting on, to tell this, one
+ * can pass a second parameter, and assign that it's relativeTo.
+ *
+ * One can get the route that this component is relative to by using the ActivatedRoute.
+ *
+ * ActivatedRoute inject the route this component is loaded, and have a lot of meta data about this
+ */
+
