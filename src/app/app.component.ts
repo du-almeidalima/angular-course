d5f6d0192ae0ad6c50e9009f1909d529cda50599
Xenosgrilda <dualmeidalima@gmail.com>
2019-07-21 13:55:53 -0300

    Section 11: Using the Fake Auth Service to Login and Logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthService} from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private authService: AuthService;
+
+  constructor(authService: AuthService) {
+    this.authService = authService;
+  }
+
+  onLoginClick(): void {
+    this.authService.login();
+  }
+
+  onLogoutClick(): void {
+    this.authService.logout();
+  }
 }
