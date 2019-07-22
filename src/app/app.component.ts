8a55caee16fc1a0bb825ccd7ecab4dd4e629cbae
Xenosgrilda <dualmeidalima@gmail.com>
2019-07-22 20:07:29 -0300

    Section 12: Setting up Routes and Adding Navigation to the App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  public activeLink = 'recipes';
-
-  public onLinkClickHandler(link: string){
-    this.activeLink = link;
-  }
-}
+export class AppComponent {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,13 +4,4 @@ import {Component, EventEmitter, Output} from "@angular/core";
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-
-  @Output()
-  public linkClicked = new EventEmitter<string>();
-
-  public onLinkClick(link: string){
-
-    this.linkClicked.emit(link);
-  }
-}
+export class HeaderComponent {}
