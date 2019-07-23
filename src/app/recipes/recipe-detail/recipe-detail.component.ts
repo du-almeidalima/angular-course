96ac2f8cbc1c7596663d373689ceb31d24f95c44
Xenosgrilda <dualmeidalima@gmail.com>
2019-07-23 20:41:44 -0300

    Section 12: Programmatic Navigation to the Edit Page

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {RecipeModel} from "../recipe.model";
 import {ShoppingListService} from "../../services/shopping-list.service";
-import {ActivatedRoute, Data} from "@angular/router";
+import {ActivatedRoute, Data, Router} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -13,7 +13,8 @@ export class RecipeDetailComponent implements OnInit {
   // Properties
   private currentRecipe: RecipeModel;
 
-  constructor(private shoppingListService: ShoppingListService, private route: ActivatedRoute) { }
+  constructor(private shoppingListService: ShoppingListService,
+              private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     // We're receiving this "data" from the resolver in the RouterModule
@@ -31,5 +32,14 @@ export class RecipeDetailComponent implements OnInit {
   // Methods
   private onSendToShoppingList(): void {
     this.shoppingListService.addIngredientsArray(this.currentRecipe.ingredients);
+  }
+
+  // This method is just for demonstrating the approach for navigating programmatically, usually you'd use:
+  // - routerLink to navigate on click
+  // - programmatically navigation to execute some logic before doing so
+  // - resolvers to fetch data that te component being loaded will need
+  private onEditRecipe(): void{
+    this.router.navigate(['../', 'edit', this.currentRecipe.id], {relativeTo: this.route});
+    // this.route = <domain>/recipes/edit/2
   }
 }
