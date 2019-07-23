569c1fecf2837dcdab01e829bc75008a364da300
Xenosgrilda <dualmeidalima@gmail.com>
2019-07-23 19:56:57 -0300

    Section 12: Retrieving Route Parameters

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-edit',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeEditComponent implements OnInit {
 
-  constructor() { }
+  private editMode: boolean;
+  private id: number;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // Adding to observable to react to changes
+    this.route.params.subscribe(
+      (params:Params) => {
+
+        this.id = params.id;
+        this.editMode = params.id != null;
+      }
+    );
   }
 
 }
+
+/* What happens here is that, whenever we assign a component to a rote and load it in a <router-outlet> we can get the
+ * route this component is loaded and its metadata such as params, query, data ...
+ * In here, we're using a Observable, which is from Reactive Programming, basically when we subscribe to an Observable
+ * we are listening to its changes, and whenever it changes we'll execute a anonymous function
+ */
