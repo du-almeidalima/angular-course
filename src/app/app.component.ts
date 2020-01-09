2421761fcbb3885b8402e432a1abf8cb4efcad2e
Xenosgrilda <dualmeidalima@gmail.com>
2020-01-09 18:09:48 -0300

    Section 15: (TD) Set Default Values with ngModel Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent {
 
+  public defaultSecretSelect = 'teacher';
+
   public onSubmit(form: NgForm): void {
     console.log(form);
   }
