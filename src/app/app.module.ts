4f1cdbd5c1c873e9243acf267c5c2cca3b74058f
Xenosgrilda <dualmeidalima@gmail.com>
2020-02-02 22:17:05 -0300

    Section 16: Validating User Input ( Custom MessageValidator Component )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent} from "./header/header.component";
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {RecipesComponent} from './recipes/recipes.component';
+import {RecipesListComponent} from './recipes/recipes-list/recipes-list.component';
+import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
+import {RecipeItemComponent} from './recipes/recipes-list/recipe-item/recipe-item.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { DropdownDirective } from "./shared/dropdown.directive";
-import { AppRoutesModule } from "./app-routes.module";
-import { HomeComponent } from './home/home.component';
-import { NoRecipeComponent } from './recipes/no-recipe/no-recipe.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import {DropdownDirective} from "./shared/dropdown.directive";
+import {AppRoutesModule} from "./app-routes.module";
+import {HomeComponent} from './home/home.component';
+import {NoRecipeComponent} from './recipes/no-recipe/no-recipe.component';
+import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
+import { ControlMessagesComponent } from './common-components/control-messages/control-messages.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     DropdownDirective,
     HomeComponent,
     NoRecipeComponent,
-    RecipeEditComponent
+    RecipeEditComponent,
+    ControlMessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
-import {AbstractControl, FormArray, FormControl, FormGroup} from "@angular/forms";
+import {AbstractControl, FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 import {RecipeService} from "../../services/recipe.service";
 import {RecipeModel} from "../recipe.model";
+// @ts-ignore
+import FOOD_PLACEHOLDER from "../../../assets/img/food-placeholder.jpg";
 
 @Component({
   selector: 'app-recipe-edit',
@@ -11,10 +13,11 @@ import {RecipeModel} from "../recipe.model";
 })
 export class RecipeEditComponent implements OnInit {
 
-  private editMode: boolean;
   private id: number;
 
   public recipeForm: FormGroup;
+  public imgPlaceholder = false;
+  public FOOD_PLACEHOLDER = FOOD_PLACEHOLDER;
 
   // Form Controls Getters
   public get name(): AbstractControl { return this.recipeForm.get('name'); }
@@ -31,7 +34,6 @@ export class RecipeEditComponent implements OnInit {
       (params:Params) => {
 
         this.id = +params.id;
-        this.editMode = params.id != null;
 
         // If id exist (edit mode) then fetch it, otherwise create new
         this.initForm(this.recipeService.getRecipe(this.id) || new RecipeModel());
@@ -46,8 +48,8 @@ export class RecipeEditComponent implements OnInit {
   public onAddIngredient(): void {
     (this.recipeForm.get('ingredients') as FormArray).push(
       new FormGroup({
-        ingName: new FormControl(),
-        ingAmount: new FormControl()
+        ingName: new FormControl(null, Validators.required),
+        ingAmount: new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
       })
     )
   }
@@ -56,17 +58,22 @@ export class RecipeEditComponent implements OnInit {
   private initForm(recipe: RecipeModel): void {
 
     this.recipeForm = new FormGroup({
-      name: new FormControl(recipe.name),
-      description: new FormControl(recipe.description),
-      imagePath: new FormControl(recipe.imagePath),
+      name: new FormControl(recipe.name, Validators.required),
+      description: new FormControl(recipe.description, Validators.required),
+      imagePath: new FormControl(recipe.imagePath || FOOD_PLACEHOLDER, {updateOn: "blur"}),
       // Mapping Ingredients to a FormGroup
       ingredients: new FormArray(recipe.ingredients.map(ing => {
         return new FormGroup({
-          ingName: new FormControl(ing.name),
-          ingAmount: new FormControl(ing.amount)
+          ingName: new FormControl(ing.name, Validators.required),
+          ingAmount: new FormControl(ing.amount, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
         })
       }))
     });
+
+    // On Image Change
+    this.imgPath.valueChanges.subscribe(data => {
+      this.imgPlaceholder = data === null || data.trim() === '';
+    })
   }
 }
 
@@ -79,4 +86,9 @@ export class RecipeEditComponent implements OnInit {
 /*
  * For building the Reactive Form it's needed to know if the form is new or already exists (to fill the fields), for that
  * we can use the "editMode"
+ */
+
+/*
+ *  "imagePath: new FormControl(recipe.imagePath || FOOD_PLACEHOLDER, {updateOn: "blur"})"
+ * This will tell Angular to only run validation on this control after the user blur
  */
