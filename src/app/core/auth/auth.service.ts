0b0ae45fdae7319eaa28e29b5732bfa250ca12db
Xenosgrilda <dualmeidalima@gmail.com>
2020-03-14 19:45:06 -0300

    Section 19: Sending the Sign Up Request

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {AuthResponseData} from "../../shared/models/response-data.model";
+import {Observable} from "rxjs";
 
 /**
  * @author Eduardo Lima
@@ -12,21 +13,18 @@ import {AuthResponseData} from "../../shared/models/response-data.model";
  */
 @Injectable({providedIn: "root"})
 export class AuthService {
-  private readonly AUTH_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
+  private readonly AUTH_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp';
   private readonly API_KEY = 'AIzaSyCpd5DSsqbLJfu6LL-7JIGzSaAGuwiVy_Y';
 
   constructor(private http: HttpClient) {}
 
-  public signup(email: string, password: string): void {
-    this.http.post(`${this.AUTH_URL}?key=${this.API_KEY}`,
+  public signUp(email: string, password: string): Observable<AuthResponseData> {
+    return this.http.post<AuthResponseData>(`${this.AUTH_URL}?key=${this.API_KEY}`,
       {
         email,
         password,
         returnSecureToken: true
       }
     )
-    .subscribe((resp: AuthResponseData) => {
-      console.log(resp)
-    })
   }
 }
