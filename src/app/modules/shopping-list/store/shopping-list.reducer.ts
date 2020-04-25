8119cb89249d50399953c66d6de2e5736a8a3adc
Xenosgrilda <dualmeidalima@gmail.com>
2020-04-25 14:42:51 -0300

    Section 24: Shopping-List Action Type and Payload Implementation

---
+++ src/app/modules/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,47 @@
+import { Action } from "@ngrx/store";
+
+import { Ingredient } from "../../../shared/models/ingredient.model";
+import { ADD_INGREDIENT } from "./shopping-list.actions";
+
+/* The initial state of this feature */
+const initState = {
+  ingredients: [
+    new Ingredient('Tomato', 4),
+    new Ingredient('Onion', 10)
+  ]
+}
+
+/**
+ * The reducer will map the action.type and return a new copy of the state with the action specific alterations.
+ * @param state Current State
+ * @param {Action} action The action type and the payload
+ */
+const shoppingListReducer = (state = initState, action: Action) => {
+  switch (action.type) {
+    case ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [
+          ...state.ingredients,
+          action
+        ]
+      }
+  }
+};
+
+export default { shoppingListReducer };
+
+/*
+ * The reducer is a function that will take the state and the action to perform the change on the application state.
+ * In ngRx Redux Reducer. This function will be called the NgRx package and this will pass 2 arguments to this func:
+ * - state: The current state before it was change, it can also receive a initial state as default value
+ * - action: The action that will be performed
+ */
+
+/*
+ * Since we're mapping the Actions.type with strings and this is pretty easier to get wrong, the actions file was
+ * created to standardize it.
+ *
+ * The Action interface just have the "type" property, to add another property, like payload, we can create a class that
+ * implements this interface and have another property. After doing this we can use our new "Action" type in the reducer
+ */
