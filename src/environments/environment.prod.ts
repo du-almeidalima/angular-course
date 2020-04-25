bd742e02784e8b96dd06db0cd5e0755c7fdcebf7
Xenosgrilda <dualmeidalima@gmail.com>
2020-04-25 11:54:52 -0300

    Section 23: Adding Firebase Host and Deploying the App

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPI: 'https://my-lists-api.firebaseio.com/',
+  firebaseAPIKey: 'AIzaSyCpd5DSsqbLJfu6LL-7JIGzSaAGuwiVy_Y'
 };
