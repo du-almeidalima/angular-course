8deefb4aa8cc93a0f8966bf809726c7e27815130
Eduardo Lima <dualmeidalima@gmail.com>
2022-04-30 12:40:11 -0300

    Note about Map and Higher Order Mapping observables

--- README.md
+++ README.md
@@ -3,21 +3,22 @@
 This repository is dedicated to my learning journey in Angular, in here as described below, all the lessons are sorted as commits.
 The Cooking App was the project that assembled all the knowledge gathered.
 
-## Angular Features used:
+## Angular Features used
 
-* Components (Data biding, Dynamic, Projection...)
-* Services
-* Directives
-* Modules
-* HttpClient
-* RxJs (Observables, Subjects and Operators)
-* Forms (Template Driven and Reactive Approach)
-* Routes
-* Redux with NgRx
+- Components (Data biding, Dynamic, Projection...)
+- Services
+- Directives
+- Modules
+- HttpClient
+- RxJs (Observables, Subjects and Operators)
+- Forms (Template Driven and Reactive Approach)
+- Routes
+- Redux with NgRx
 
 This project is hosted in [Firebase Hosting](https://firebase.google.com/docs/hosting) and the live version can be found [here](https://my-lists-api.web.app/home), and you can log in with
-* email: testuser@test.com
-* password: 123456
+
+- email: testuser@test.com
+- password: 123456
 
 I had a great time learning this framework and this repository will be used for consult the learned content.
 
@@ -25,28 +26,27 @@ I had a great time learning this framework and this repository will be used for 
 
 The lessons will be organized as commits, for instance:
 The Section 2: The Basics will be
-```
-  "[Section 'number'] : [lesson name]"
-```
+
+`"[Section 'number'] : [lesson name]"`
+
 ## Cooking App
 
-The <b>Cooking App</b> is a project to practice what was being learned, It consists of a enhanced shopping list where you can add recipes with ingredients to make the shopping list creation easier. All the data and authentication is maintained in <b>Firebase</b>, you can upload your data a fetch it from there. 
+The **Cooking App** is a project to practice what was being learned, It consists of a enhanced shopping list where you can add recipes with ingredients to make the shopping list creation easier. All the data and authentication is maintained in **Firebase**, you can upload your data a fetch it from there.
 
 It's structured as depicted below.
 
 ### App Planning
+
 ![App Diagram](src/assets/readme/app-planning.png?raw=true "App Diagram")
 
 ### State Management Approach
+
 The initial approach was to use Services with Observables for managing the app state. This was a fine solution, but for scalability purposes
 I decided to change the pattern to the [Redux](https://redux.js.org/) approach by using [NgRx](https://ngrx.io/) which is inspired by the
 Redux pattern by with some advantages for Angular apps.
 
 ![NgRx State Management](src/assets/readme/state-management-ngrx.png?raw=true "NgRx State Management")
 
-
-
-
 ## Author
 
-* **Eduardo Lima** - du-almeidalima
+- **Eduardo Lima** - du-almeidalima
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,3 +53,41 @@ export class AppModule { }
  * After importing StoreMode and the Reducers, NgRx will set a Application Store for us, registering our reducers.
  * Now any actions can reach our reducers.
  */
+
+
+/*
+ * About RxJs Map and High Order Mapping operators
+ *
+ * - Concat: Similar to array concat. It will take two or more observables, subscribe to the first one,
+ * map its emitted values and wait for it to complete before subscribing to the next one.
+ * - ConcatMap: will take multiple observables and map each of them into another observable and wait for
+ * each to complete before subscribing to the next one.
+ *
+ *
+ * - Merge: As its name implies, it will merge two or more observables and subscribe to all of them,
+ * and then it outputs the values of each source Observable to the combined result Observable as the multiple values
+ * arrive over time. If one of the observables completes, the merge operator will still emit values of the remaining.
+ * The result will not complete until ALL the merged Observables are completed.
+ * - MergeMap: Very similar to ConcatMap, the MergeMap can subscribe to many observables and map them into others. For
+ * example, take the $formValues and mapping into $httpRequest. But it will not do it sequentially, waiting for the
+ * previous to finish. It will subscribe to all of them at the same time, and map the values into new observables as
+ * those values are emitted. In another words, in parallel.
+ *
+ *
+ * - Switch: Similar to Concat, but instead of waiting for the previous Observable to complete in order to subscribe to the
+ * next one. It unsubscribes to the previous at the moment a new one arrives.
+ * - SwitchMap: Applying the ideia of Switch, let's say the $formValues emits one value and the switchMap takes this value
+ * and maps it into another observable (e.g. $httpRequest) and subscribes to it. However, before this observable finishes,
+ * the $formValues emits another value. Then the switchMap will unsubscribe to the first and subscribe to the new one,
+ * returning a new Observable.
+ *
+ *
+ * - Exhaust: Can be thought of as a blend of Switch and Concat. The exhaust can take multiple Observables, but it will one
+ * handle one at the time, similar to Concat. HOWEVER, if a new observable comes while the previous hasn't completed, it will
+ * not queue it, like Concat. But it will just ignore it. Then once the first has completed, if a new arrives, it will be
+ * subscribed.
+ * ExhaustMap: Using an example, the $formValues emits one value, then this Observable is mapped by ExhaustMap into another
+ * Observable, like $httpRequest. But, while the $httpRequest hasn't completed, another value is emitted by $formValues.
+ * Instead of unsubscribing to it like the SwitchMap, the ExhaustMap will ignore incoming subscriptions while there's one
+ * already subscribed and not completed.
+ */
